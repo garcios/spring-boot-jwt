9ea2340899a8187089acc2f967124398d51ea4bb
garcios <cm1017pg@outlook.com>
2017-03-28 22:12:11 +0800

    Update UserController.java

--- src/main/java/com/oscar/jwt/UserController.java
+++ src/main/java/com/oscar/jwt/UserController.java
@@ -1,5 +1,6 @@
 package com.oscar.jwt;
 
+import org.springframework.security.access.annotation.Secured;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -9,8 +10,22 @@ public class UserController {
 
     /* Maps to all HTTP actions by default (GET,POST,..)*/
     @RequestMapping("/users")
+    @Secured("ROLE_ADMIN")
     public @ResponseBody String getUsers() {
         return "{\"users\":[{\"firstname\":\"Richard\", \"lastname\":\"Feynman\"}," +
                 "{\"firstname\":\"Marie\",\"lastname\":\"Curie\"}]}";
     }
+    
+    
+    @RequestMapping("/dummy1")
+    public @ResponseBody String getDummy1() {
+        return "{\"mydata\": \"Testing1\"}";
+    }
+
+    @RequestMapping("/dummy2")
+    @Secured("ROLE_USER")
+    public @ResponseBody String getDummy2() {
+        return "{\"mydata\": \"Testing2\"}";
+    }
+
 }
